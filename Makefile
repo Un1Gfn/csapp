52a234212d5e20757018cc7d8c52dfda0f3e38a5
Darren Ng <darren19970810@gmail.com>
2021-01-08 13:42:50 +0800

    w/ libelf

--- Makefile
+++ Makefile
@@ -15,9 +15,10 @@ p68_float_overflow.out:
 p69_float_not_associative.out:
 %.out: %.c ; $(CC) $(CFLAGS) -o $@ $<
 
-p76_prog_32.out: T:=-m32
-p76_prog_64.out: T:=-m64
-p76_prog_%.out: p76_prog.c ; $(CC) $(CFLAGS) $(T) -o $@ $<
+p76_prog_32.out: M:=-m32
+p76_prog_64.out: M:=-m64
+p76_prog_%.out: p76_prog.c ; $(CC) $(CFLAGS) $(M) $(shell pkg-config --libs libelf,libbsd) -o $@ $<
+# p76_prog_%.out: p76_prog.c ; $(CC) $(CFLAGS) $(M) -ldw -lelf -o $@ $<
 p76_prog_run: p76_prog_32.out p76_prog_64.out
 	@echo
 	@./p76_prog_32.out
--- p76_prog.c
+++ p76_prog.c
@@ -1,4 +1,6 @@
 // https://stackoverflow.com/questions/34960383
+// https://github.com/sergev/elftoolchain/tree/master/documentation/libelf-by-example
+// https://atakua.org/old-wp/wp-content/uploads/2015/03/libelf-by-example-20100112.pdf
 
 #include <assert.h>
 #include <unistd.h>
@@ -6,25 +8,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 // #include <linux/elf.h>
-#include <elf.h>
+// #include <elf.h>
+#include <gelf.h>
+#include <libelf.h>
 
-#if defined (__LP64__)
-typedef Elf64_Ehdr ElfN_Ehdr;
-#define FMT "0x%lX"
-#else
-typedef Elf32_Ehdr ElfN_Ehdr;
-#define FMT "0x%X"
-#endif
+#define LIBBSD_NETBSD_VIS
+#include <bsd/vis.h>
 
 #define SZ 1024
+int fd=-1;
+char path[SZ]={};
+#define path_load() assert(1<=readlink("/proc/self/exe",path,SZ));printf("%s\n",path)
+#define fd_open() assert(1<=(fd=open(path,O_RDONLY)))
+#define fd_close() assert(0==close(fd));fd=-1
 
-int main(){
+void plain(){
 
-  char path[SZ]={};
-  assert(1<=readlink("/proc/self/exe",path,SZ));
-  printf("%s\n",path);
-  int fd=-1;
-  assert(1<=(fd=open(path,O_RDONLY)));
+  #if defined (__LP64__)
+  typedef Elf64_Ehdr ElfN_Ehdr;
+  #define FMT "0x%lX"
+  #else
+  typedef Elf32_Ehdr ElfN_Ehdr;
+  #define FMT "0x%X"
+  #endif
+
+  fd_open();
 
   ElfN_Ehdr header={};
   read(fd,&header,sizeof(ElfN_Ehdr));
@@ -70,6 +78,7 @@ int main(){
   printf("entry point "FMT"\n",header.e_entry);
   printf("program header table offset "FMT"\n",header.e_phoff);
   printf("section header table offset "FMT"\n",header.e_shoff);
+  #undef FMT
 
   // printf("0x%X\n",header.e_flags);
   assert(header.e_flags==0);
@@ -91,10 +100,78 @@ int main(){
     header.e_shentsize*header.e_phnum
   );
 
-  printf("\n");
+  // printf("\n");
 
-  assert(0==close(fd));
-  fd=-1;
+  fd_close();
+
+}
+
+void libelf(){
+
+  fd_open();
+  // printf("%s\n",elf_errmsg(-1));
+  assert(EV_CURRENT==elf_version(EV_CURRENT));
+  Elf *e=elf_begin(fd,ELF_C_READ,NULL);
+  // assert(e);
+  if(!e){
+    printf("%s\n",elf_errmsg(-1));
+    assert(false);
+  }
+
+  assert(ELF_K_ELF==elf_kind(e));
+  #if defined (__LP64__)
+  #define FMT "0x%lX"
+  #else
+  #define FMT "0x%llX"
+  #endif
+  printf("base offset "FMT"\n",elf_getbase(e));
+  #undef FMT
+
+  char *id=elf_getident(e,NULL);
+  assert(id);
+  static_assert(EI_ABIVERSION==(9-1)&&EI_PAD==(10-1));
+  char dst[SZ]={};
+  /*for(int i=0;i<EI_PAD;++i){
+    vis(dst,id[i],0,'\0');
+    printf("%s ",dst);
+  }
+  printf("\n");*/
+  assert(1<=strvisx(dst,id,EI_PAD,VIS_CSTYLE|VIS_OCTAL));
+  printf("%s\n",dst);
+  id=NULL;
+
+  const int class=gelf_getclass(e);
+  if(class==ELFCLASS32){
+    Elf32_Ehdr *ee=elf32_getehdr(e);
+    assert(ee);
+    ee=NULL;
+  }else if(class==ELFCLASS64){
+    Elf64_Ehdr *ee=elf64_getehdr(e);
+    assert(ee);
+    ee=NULL;
+  }else{
+    assert(false);
+  }
+
+  size_t t=0;
+  assert(0==elf_getphdrnum(e,&t));
+  printf("%zu program headers\n",t);
+  assert(0==elf_getshdrnum(e,&t));
+  printf("%zu sections\n",t);
+
+  assert(0==elf_end(e));
+  e=NULL;
+  fd_close();
+
+}
+
+int main(){
+
+  path_load();
+  printf(".\n");
+  plain();
+  printf(".\n");
+  libelf();
 
   return 0;
 }
